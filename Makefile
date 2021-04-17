572d64cd8cceac486caa1302ee7709ad9d12a9cf
Harshal5 <harshalnpatil5@gmail.com>
2021-04-18 00:58:47 +0530

    gettid added: test done

--- Makefile
+++ Makefile
@@ -183,6 +183,7 @@ UPROGS=\
 	_zombie\
 	_clonetest\
 	_jointest\
+	_gettidtest\
 
 fs.img: mkfs README $(UPROGS)
 	./mkfs fs.img README $(UPROGS)
@@ -255,7 +256,7 @@ EXTRA=\
 	printf.c umalloc.c\
 	README dot-bochsrc *.pl toc.* runoff runoff1 runoff.list\
 	.gdbinit.tmpl gdbutil\
-	clonetest.c jointest.c\ 
+	clonetest.c jointest.c gettidtest.c\ 
 
 dist:
 	rm -rf dist
---
+++ gettidtest.c
@@ -0,0 +1,24 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+void func(void *arg1, void *arg2)
+{
+  printf(1, "Child pid = %d\n", getpid());
+  printf(1, "Child tid = %d\n", gettid());
+  exit();
+}
+
+int main(int argc, char *argv[])
+{
+  void *stack_bottom = malloc(4096);
+  int arg1 = 0;
+  char *arg2 = "Hello";
+  clone(&func, &arg1, arg2, stack_bottom);
+  join(stack_bottom);
+  printf(1, "Parent pid = %d\n", getpid());
+  printf(1, "Parent tid = %d\n", gettid());
+  printf(1, "All threads joined");
+  printf(1, "END\n");
+  exit();
+}
